2c56d6de1faf4f606aaaa0d33bb9be8c9a1fd2a1
Kinnunen Samuli TTK15SP <samulikinnunen@kamk.fi>
2016-11-05 12:59:19 +0200

    Mapped HOTAS

---
+++ Assets/Scripts/Input/InputTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputTest : MonoBehaviour
+{
+    float pitch = 0f;
+    float roll = 0f;
+    float yaw = 0f;
+    float gas = 0f;
+
+    bool resetCamera = false;
+    bool breathFire = false;
+    bool fartOil = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        pitch = Input.GetAxis("Pitch");
+        roll = Input.GetAxis("Roll");
+        yaw = Input.GetAxis("Yaw");
+        gas = Input.GetAxis("Gas");
+
+        resetCamera = Input.GetButton("ResetCamera");
+        breathFire = Input.GetButton("BreathFire");
+        fartOil = Input.GetButton("FartOil");
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 500, 2000), "Pitch - " + pitch + "\nRoll - " + roll + "\nYaw - " + yaw + "\nGas " + gas
+            + "\n\nResetCamera: " + resetCamera + "\nBreathFire: " + breathFire + "\nFartOil: " + fartOil);
+    }
+}
