3d6bc11b7b0f450a61dff76b7d3154abd78da70f
Samuli <jaaskelainen.samuli@gmail.com>
2016-11-06 10:21:56 +0200

    Updated prefab layers & added score manager

--- Assets/Scripts/Burnable.cs
+++ Assets/Scripts/Burnable.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Burnable : MonoBehaviour
 {
+	public int score = 1;
+
 	public void Burn()
 	{
+		ScoreManager.Instance.AddScore(score);
 		Destroy (gameObject);
 	}
    
